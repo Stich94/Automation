d33e2ff88b940f87b7965c4003655a1c6b87941a
BernhardW <bernhard.woltron@gmail.com>
2022-03-04 18:36:07 +0100

    iterable

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello")
+
+for item in (1, 2, 3, 4):
+    print(item)
+
+user = {
+    "name": "Golem",
+    "age": 45,
+    "can_swim": False
+}
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
+
+for item in user.items():
+    print(item)
+
+for key, value in user.items():
+    print(key, value)
+
+print(user["name"])
